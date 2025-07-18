caa8357a64e3b6e98550d6dd86ace73c9deaf0d8
Will D <diweizhi@gmail.com>
2025-07-18 13:26:08 +1000

    login hook

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
+
+export const useLogin = () => {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { dispatch } = useAuthContext();
+
+  const login = async (email, password) => {
+    setIsLoading(true);
+    setError(null);
+    const response = await fetch('/api/user/login', {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+    const json = response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(json.error);
+    }
+    if (response.ok) {
+      setIsLoading(false);
+      dispatch({ type: 'LOGIN', payload: json });
+    }
+  };
+  return { error, isLoading, login };
+};
