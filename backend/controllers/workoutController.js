a5ca01e886fc9bca6df692f0b254e5d743eba664
Will D <diweizhi@gmail.com>
2025-08-07 22:50:09 +1000

    getWorkouts

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -1,4 +1,13 @@
-const getWorkouts = async (req, res) => {};
+const Workout = require('../models/workoutModel');
+
+const getWorkouts = async (req, res) => {
+  try {
+    const workouts = await Workout.find().sort({ createdAt: -1 });
+    res.status(200).json(workouts);
+  } catch (error) {
+    res.status(400).json({ error: json.error });
+  }
+};
 const getWorkout = async (req, res) => {};
 
 const postWorkout = async (req, res) => {};
