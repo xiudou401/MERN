aaf42e5dd3364ba7f9774c9cf3c25a726cbe1529
Will D <diweizhi@gmail.com>
2025-08-07 22:57:35 +1000

    postWorkout

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -5,12 +5,28 @@ const getWorkouts = async (req, res) => {
     const workouts = await Workout.find().sort({ createdAt: -1 });
     res.status(200).json(workouts);
   } catch (error) {
-    res.status(400).json({ error: json.error });
+    res.status(400).json({ error: error.message });
   }
 };
-const getWorkout = async (req, res) => {};
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const workout = await Workout.findById(id);
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-const postWorkout = async (req, res) => {};
+const postWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 const putWorkout = async (req, res) => {};
 const deleteWorkout = async (req, res) => {};
 
