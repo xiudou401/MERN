6244fba36788422259d08aa853647dbcb5c75938
Will D <diweizhi@gmail.com>
2025-08-07 22:23:01 +1000

    basic settings done

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,14 @@
+const getWorkouts = async (req, res) => {};
+const getWorkout = async (req, res) => {};
+
+const postWorkout = async (req, res) => {};
+const putWorkout = async (req, res) => {};
+const deleteWorkout = async (req, res) => {};
+
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  postWorkout,
+  putWorkout,
+  deleteWorkout,
+};
