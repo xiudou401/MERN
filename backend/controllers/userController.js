52f920e6601dc30b62b999ad8bbef0622575fee8
Will D <diweizhi@gmail.com>
2025-08-08 10:53:32 +1000

    login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,6 +16,16 @@ const signupUser = async (req, res) => {
   }
 };
 
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    console.log(token);
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = { signupUser, loginUser };
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -37,4 +37,22 @@ userSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+userSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw new Error('all fields must be filled');
+  }
+  const user = await this.findOne({ email });
+
+  if (!user) {
+    throw new Error('no such user');
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw new Error('incorrect password');
+  }
+  return user;
+};
+
 module.exports = mongoose.model('User', userSchema);
