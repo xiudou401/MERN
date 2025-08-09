44150828008cae4e84748f88184f250cdc186f91
Will D <diweizhi@gmail.com>
2025-08-09 23:08:20 +1000

    emptyFields

--- backend/controllers/workoutController.js
+++ backend/controllers/workoutController.js
@@ -23,6 +23,24 @@ const postWorkout = async (req, res) => {
   const { title, load, reps } = req.body;
   const user_id = req.user._id;
 
+  const emptyFields = [];
+
+  if (!title) {
+    emptyFields.push('title');
+  }
+  if (!reps) {
+    emptyFields.push('reps');
+  }
+  if (!load) {
+    emptyFields.push('load');
+  }
+
+  if (emptyFields.length > 0) {
+    return res
+      .status(400)
+      .json({ error: 'Please fill in all the fields', emptyFields });
+  }
+
   try {
     const workout = await Workout.create({ title, load, reps, user_id });
     res.status(200).json(workout);
